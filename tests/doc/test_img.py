e3d277c8bff3381a58222b1ecbf5fce1002bff29
alcholiclg <ligongshengzju@foxmail.com>
2025-07-22 11:41:35 +0800

    add examples in unittest

--- tests/doc/test_img.py
+++ tests/doc/test_img.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+from typing import List
 
 from ms_agent.tools.docling.doc_loader import DocLoader
 
@@ -10,10 +11,18 @@ os.environ['TEST_LEVEL'] = '1'
 
 class TestExtractImage(unittest.TestCase):
     base_dir: str = os.path.dirname(os.path.abspath(__file__))
-    absolute_path_img_url: str = 'https://www.chinahighlights.com/hangzhou/food-restaurant.htm'
-    relative_path_img_url: str = 'https://github.com/asinghcsu/AgenticRAG-Survey'
-    figure_tag_img_url: str = 'https://blogs.nvidia.com/blog/what-is-retrieval-augmented-generation/'
-    data_uri_img_url: str = 'https://arxiv.org/html/2505.16120v1'
+    absolute_path_img_url: List[str] = [
+        'https://www.chinahighlights.com/hangzhou/food-restaurant.htm'
+    ]
+    relative_path_img_url: List[str] = [
+        'https://github.com/asinghcsu/AgenticRAG-Survey',
+        'https://www.ams.org.cn/article/2021/0412-1961/0412-1961-2021-57-11-1343.shtml',
+        'https://arxiv.org/html/2502.15214'
+    ]
+    figure_tag_img_url: List[str] = [
+        'https://blogs.nvidia.com/blog/what-is-retrieval-augmented-generation/'
+    ]
+    data_uri_img_url: List[str] = ['https://arxiv.org/html/2505.16120v1']
 
     @unittest.skipUnless(test_level() >= 0, 'skip test in current test level')
     def test_absolute_path_img(self):
@@ -21,15 +30,19 @@ class TestExtractImage(unittest.TestCase):
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
         doc_loader = DocLoader()
-        doc_results = doc_loader.load(
-            urls_or_files=[self.absolute_path_img_url])
-        for idx, pic in enumerate(doc_results[0].pictures):
-            print(f'Picture: {pic.self_ref} ...')
-            if pic.image:
-                pic.image.pil_image.save(
-                    os.path.join(save_dir, 'picture_' + str(idx) + '.png'))
-        assert len(
-            doc_results[0].pictures) > 0, 'No pictures found in the document.'
+        doc_results = doc_loader.load(urls_or_files=self.absolute_path_img_url)
+        for task_id, doc in enumerate(doc_results):
+            task_save_dir = os.path.join(save_dir, f'task_{task_id}')
+            if not os.path.exists(task_save_dir):
+                os.makedirs(task_save_dir)
+            print(f'Task {task_id} ...')
+            for idx, pic in enumerate(doc.pictures):
+                print(f'Picture: {pic.self_ref} ...')
+                if pic.image:
+                    pic.image.pil_image.save(
+                        os.path.join(task_save_dir,
+                                     'picture_' + str(idx) + '.png'))
+            assert len(doc.pictures) > 0, 'No pictures found in the document.'
 
     @unittest.skipUnless(test_level() >= 0, 'skip test in current test level')
     def test_relative_path_img(self):
@@ -37,15 +50,19 @@ class TestExtractImage(unittest.TestCase):
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
         doc_loader = DocLoader()
-        doc_results = doc_loader.load(
-            urls_or_files=[self.relative_path_img_url])
-        for idx, pic in enumerate(doc_results[0].pictures):
-            print(f'Picture: {pic.self_ref} ...')
-            if pic.image:
-                pic.image.pil_image.save(
-                    os.path.join(save_dir, 'picture_' + str(idx) + '.png'))
-        assert len(
-            doc_results[0].pictures) > 0, 'No pictures found in the document.'
+        doc_results = doc_loader.load(urls_or_files=self.relative_path_img_url)
+        for task_id, doc in enumerate(doc_results):
+            task_save_dir = os.path.join(save_dir, f'task_{task_id}')
+            if not os.path.exists(task_save_dir):
+                os.makedirs(task_save_dir)
+            print(f'Task {task_id} ...')
+            for idx, pic in enumerate(doc.pictures):
+                print(f'Picture: {pic.self_ref} ...')
+                if pic.image:
+                    pic.image.pil_image.save(
+                        os.path.join(task_save_dir,
+                                     'picture_' + str(idx) + '.png'))
+            assert len(doc.pictures) > 0, 'No pictures found in the document.'
 
     @unittest.skipUnless(test_level() >= 2, 'skip test in current test level')
     def test_figure_tag_img(self):
@@ -53,14 +70,19 @@ class TestExtractImage(unittest.TestCase):
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
         doc_loader = DocLoader()
-        doc_results = doc_loader.load(urls_or_files=[self.figure_tag_img_url])
-        for idx, pic in enumerate(doc_results[0].pictures):
-            print(f'Picture: {pic.self_ref} ...')
-            if pic.image:
-                pic.image.pil_image.save(
-                    os.path.join(save_dir, 'picture_' + str(idx) + '.png'))
-        assert len(
-            doc_results[0].pictures) > 0, 'No pictures found in the document.'
+        doc_results = doc_loader.load(urls_or_files=self.figure_tag_img_url)
+        for task_id, doc in enumerate(doc_results):
+            task_save_dir = os.path.join(save_dir, f'task_{task_id}')
+            if not os.path.exists(task_save_dir):
+                os.makedirs(task_save_dir)
+            print(f'Task {task_id} ...')
+            for idx, pic in enumerate(doc.pictures):
+                print(f'Picture: {pic.self_ref} ...')
+                if pic.image:
+                    pic.image.pil_image.save(
+                        os.path.join(task_save_dir,
+                                     'picture_' + str(idx) + '.png'))
+            assert len(doc.pictures) > 0, 'No pictures found in the document.'
 
     @unittest.skipUnless(test_level() >= 0, 'skip test in current test level')
     def test_data_uri_img(self):
@@ -68,14 +90,19 @@ class TestExtractImage(unittest.TestCase):
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
         doc_loader = DocLoader()
-        doc_results = doc_loader.load(urls_or_files=[self.data_uri_img_url])
-        for idx, pic in enumerate(doc_results[0].pictures):
-            print(f'Picture: {pic.self_ref} ...')
-            if pic.image:
-                pic.image.pil_image.save(
-                    os.path.join(save_dir, 'picture_' + str(idx) + '.png'))
-        assert len(
-            doc_results[0].pictures) > 0, 'No pictures found in the document.'
+        doc_results = doc_loader.load(urls_or_files=self.data_uri_img_url)
+        for task_id, doc in enumerate(doc_results):
+            task_save_dir = os.path.join(save_dir, f'task_{task_id}')
+            if not os.path.exists(task_save_dir):
+                os.makedirs(task_save_dir)
+            print(f'Task {task_id} ...')
+            for idx, pic in enumerate(doc.pictures):
+                print(f'Picture: {pic.self_ref} ...')
+                if pic.image:
+                    pic.image.pil_image.save(
+                        os.path.join(task_save_dir,
+                                     'picture_' + str(idx) + '.png'))
+            assert len(doc.pictures) > 0, 'No pictures found in the document.'
 
 
 if __name__ == '__main__':
