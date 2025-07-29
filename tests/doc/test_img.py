ed1975d03d8f91fbcf0c9c6b8efdd876edfc91a6
luyan <suluyan.sly@aliabab-inc.com>
2025-07-29 15:40:47 +0800

    tmp for test

--- tests/doc/test_img.py
+++ tests/doc/test_img.py
@@ -15,7 +15,7 @@ class TestExtractImage(unittest.TestCase):
     figure_tag_img_url: str = 'https://blogs.nvidia.com/blog/what-is-retrieval-augmented-generation/'
     data_uri_img_url: str = 'https://arxiv.org/html/2505.16120v1'
 
-    @unittest.skipUnless(test_level() >= 0, 'skip test in current test level')
+    @unittest.skipUnless(test_level() >= 2, 'skip test in current test level')
     def test_absolute_path_img(self):
         save_dir = os.path.join(self.base_dir, 'absolute_path_img')
         if not os.path.exists(save_dir):
@@ -31,7 +31,7 @@ class TestExtractImage(unittest.TestCase):
         assert len(
             doc_results[0].pictures) > 0, 'No pictures found in the document.'
 
-    @unittest.skipUnless(test_level() >= 0, 'skip test in current test level')
+    @unittest.skipUnless(test_level() >= 2, 'skip test in current test level')
     def test_relative_path_img(self):
         save_dir = os.path.join(self.base_dir, 'relative_path_img')
         if not os.path.exists(save_dir):
@@ -62,7 +62,7 @@ class TestExtractImage(unittest.TestCase):
         assert len(
             doc_results[0].pictures) > 0, 'No pictures found in the document.'
 
-    @unittest.skipUnless(test_level() >= 0, 'skip test in current test level')
+    @unittest.skipUnless(test_level() >= 2, 'skip test in current test level')
     def test_data_uri_img(self):
         save_dir = os.path.join(self.base_dir, 'data_uri_img')
         if not os.path.exists(save_dir):
