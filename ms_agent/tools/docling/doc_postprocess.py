53b6523ddc4e2cc94c6439ae1232b055e2f886ad
alcholiclg <ligongshengzju@foxmail.com>
2025-08-14 17:50:47 +0800

    Support for filtering images using classification labels.

--- ms_agent/tools/docling/doc_postprocess.py
+++ ms_agent/tools/docling/doc_postprocess.py
@@ -22,4 +22,14 @@ class PostProcess:
                 if pic_item.image.size.height * pic_item.image.size.width < PostProcess.MIN_PICTURE_SIZE:
                     pic_item.image = None  # Remove image if too small
 
+            if hasattr(pic_item, 'annotations') and pic_item.annotations:
+                pic_classes = getattr(pic_item.annotations[0],
+                                      'predicted_classes', None)
+                pic_class = pic_classes[
+                    0].class_name if pic_classes else None  # Get the first predicted class if available
+                if pic_class is not None and pic_class.lower() in [
+                        'qr_code', 'logo', 'icon'
+                ]:
+                    pic_item.image = None
+
         return doc
