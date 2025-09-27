890d9d16b7d617e4bbaed84088f0c0093290302e
tastelikefeet <yuze.zyz@alibaba-inc.com>
2025-09-27 22:12:46 +0800

    wip

--- docs/source_en/conf.py
+++ docs/source_en/conf.py
@@ -18,10 +18,10 @@ import sys
 sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 
-project = 'swift'
+project = 'MS-Agent'
 copyright = '2022-2025, Alibaba ModelScope'
 author = 'ModelScope Authors'
-version_file = '../../swift/version.py'
+version_file = '../../ms_agent/version.py'
 html_theme = 'sphinx_rtd_theme'
 language = 'en'
 
--- docs/source_en/index.rst
+++ docs/source_en/index.rst
@@ -1,8 +1,8 @@
-.. swift documentation file,
+.. MS-Agent documentation file,
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Swift DOCUMENTATION
+MS-Agent DOCUMENTATION
 ========================
 
 .. toctree::
